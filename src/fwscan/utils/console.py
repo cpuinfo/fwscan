71fd9590b6bd2cb8aa0db5ccc17ebb48312130e0
CPU Info <cpuinfo10@gmail.com>
2021-07-25 20:47:35 +0530

    Add support for beautiful traceback

--- src/fwscan/utils/console.py
+++ src/fwscan/utils/console.py
@@ -1,6 +1,8 @@
 from rich.console import Console
+from rich.traceback import install
 from rich.theme import Theme
 
+install()
 # Rich Theme
 fwtheme = Theme({"success": "green", "error": "bold red"})
 
